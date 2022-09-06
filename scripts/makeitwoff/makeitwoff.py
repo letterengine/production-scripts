b02a3b534d178fb2f9b2f4b3e71685d81b91efcf
eduairet <hola@eduairet.com>
2022-09-05 19:05:00 -0500

    Update makeitwoff.py

--- scripts/makeitwoff/makeitwoff.py
+++ scripts/makeitwoff/makeitwoff.py
@@ -6,7 +6,7 @@ from fontTools.ttLib import TTFont
 
 def convert(fsDir: str, prFlvs: list, nwFlvs: list) -> None:
     '''This function transform your fonts from a previous flavor selected to a new one'''
-    fsDir = fsDir.replace("'", '')
+    fsDir = fsDir.replace("'", '').strip().replace('\\', '')
     try:
         for prFlv in prFlvs:
             fonts = [os.path.join(fsDir, f)
@@ -27,9 +27,9 @@ pfi = int(input(
     'From which format do you want to convert (write the number)?\n1 - otf\n2 - ttf\n3 - both\n'))
 nfi = int(input(
     'To which format do you want to convert (write the number)?\n1 - woff\n2 - woff2\n3 - both\n'))
-
 # Flavors handle
 prevFlavs = [prevFlavors[pfi-1]] if pfi != 3 else prevFlavors
 newFlavs = [newFlavors[nfi-1]] if nfi != 3 else newFlavors
+print(prevFlavs, newFlavs)
 # Script run
 convert(fontsDir, prevFlavs, newFlavs)
